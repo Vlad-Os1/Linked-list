7222ef376f0e595bafb3cc0165646636aaf9d0ea
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-06-16 20:32:15 +0300

    feat: add methods to retrieve list info - implement printSize method to return the total number of nodes in the list - implement printHead method to return the first node in the list - implement printTail method to return the last node in the list - implement printAt method to return the node at specific index like in Array

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = null;
-    this.size = 0;
+    this.listSize = 0;
   }
 
   // Adds Node in the Beginning
   prepend(value) {
-    this.head = new Node(value, this.head);
-    this.size++;
+    this.head = new Node(value, this.head); //  new head node points to the old head
+    this.listSize++;
   }
 
   // Adds Node in the End
@@ -31,7 +31,36 @@ class LinkedList {
       }
       currentNode.nextNode = node;
     }
-    this.size++;
+    this.listSize++;
+  }
+
+  //  returns the total number of nodes in the list
+  printSize() {
+    console.log(this.listSize);
+  }
+
+  // returns the first node in the list
+  printHead() {
+    console.log(this.head);
+  }
+
+  // returns the last node in the list
+  printTail() {
+    let currentNode = this.head;
+    while (currentNode.nextNode) {
+      currentNode = currentNode.nextNode;
+    }
+    console.log(currentNode);
+  }
+
+  // return the Node at specific index like in array
+  printAt(index) {
+    let currentNode = this.head;
+    if (index < 0 || index > this.listSize) return;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.nextNode;
+    }
+    console.log(currentNode);
   }
 
   // Print our list
@@ -50,4 +79,8 @@ const ll = new LinkedList();
 ll.prepend(10);
 ll.append(20);
 ll.append(30);
+// ll.printSize();
+// ll.printHead();
+// ll.printTail();
+ll.printAt(2); // returns node with value 30
 ll.toString();
